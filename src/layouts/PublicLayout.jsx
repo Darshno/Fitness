ca220c532172c1a8879f13fd24da51eb43e60997
Darshno <darshno18@gmail.com>
2026-08-25 10:34:07 +0530

    Add LanguageSwitcher and localization to PublicLayout

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -1,6 +1,10 @@
 import { Link, Outlet } from "react-router-dom";
+import LanguageSwitcher from "../components/LanguageSwitcher";
+import { useLanguage } from "../context/LanguageContext";
 
 export default function PublicLayout() {
+  const { t } = useLanguage();
+
   return (
     <div className="public-shell">
       <header className="public-header">
@@ -11,9 +15,10 @@ export default function PublicLayout() {
             <div className="brand-sub">AI Companion</div>
           </div>
         </Link>
-        <div className="btn-row">
-          <Link to="/login" className="ghost-btn">Login</Link>
-          <Link to="/signup" className="primary-btn">Get Started</Link>
+        <div className="btn-row" style={{ alignItems: "center" }}>
+          <LanguageSwitcher />
+          <Link to="/login" className="ghost-btn">{t("common.login")}</Link>
+          <Link to="/signup" className="primary-btn">{t("common.getStarted")}</Link>
         </div>
       </header>
       <Outlet />
